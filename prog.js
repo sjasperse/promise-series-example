f869f617d8fe0fa152b695da09a2fee3aaf9e059
sjasperse <sjasperse@users.noreply.github.com>
2015-06-03 10:19:49 -0400

    Adding comments

--- prog.js
+++ prog.js
@@ -1,24 +1,27 @@
 var Q = require('q');
 var _ = require('underscore');
 
+// turns a single name into a fake chunk of work, returning a promise
 function doWork(name) {
 	return function () { 
 		var deferrer = Q.defer();
 
 		setTimeout(function () {
-			deferrer.resolve();
 			console.log('Task finished: ' + name);
+			deferrer.resolve();
 		}, 1000);
 
 		return deferrer.promise;
 	};
 }
 
+// creathing the list of tasks I want to execute
 var items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 var tasks = _.map(items, function (item) {
 	return doWork(item);
 });
 
+// function to chain tasks together, returning the final promise
 function callInOrder(list) {
 	function callRecursive(head, tail) {
 		var headPromise = head();
@@ -35,6 +38,7 @@ function callInOrder(list) {
 	return callRecursive(list[0], _.rest(list));
 }
 
+// test it. should see each task finish in order, with a noticable delay between, and a 'Finished' message at the end.
 console.log('Starting tasks...');
 callInOrder(tasks)
 	.then(function () {
